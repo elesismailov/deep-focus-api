69389cb1a55362af0579e51ae063681aca77df72
eles-pc <eleswastaken@gmail.com>
2021-08-08 16:42:05 +0600

    Improve Code

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-    
+
 // var requestURL = "./data.json";
 // const request = new XMLHttpRequest();
 // request.open('GET', requestURL);
@@ -6,83 +6,72 @@
 // request.send();
 
 // request.addEventListener("load", function(){
-//     console.log(request.response)
+//     console.log(request.response.timeData)
+//     console.log(request.response.timeData)
+//     console.log(request.response.trackData)
+//     console.log(request.response.timeData)
+//     console.log(request.response.timeData)
 // })
 
 
-// const fs = require("fs");
-// let output = fs.createWriteStream("numbers.txt");
-// for(let i = 0; i < 100; i++) {
-// output.write(`${i}\n`);
-// }
-// output.end();
+let userWorkTime = 1500,
+userBreakTime = 300,
+userLongBreakTime = 900;
 
-// // const fs = require("fs");
+function changeTime() { 
+    userWorkTime = Number(document.querySelector("#pomodoroTime").value)*60;
+    userBreakTime = Number(document.querySelector("#breakTime").value)*60;
+    userLongBreakTime = Number(document.querySelector("#longBreakTime").value)*60;
+    document.querySelector("#pomodoroTimeMin").innerHTML =  document.querySelector("#pomodoroTime").value;
+    document.querySelector("#breakTimeMin").innerHTML =  document.querySelector("#breakTime").value;
+    document.querySelector("#longBreakTimeMin").innerHTML =  document.querySelector("#longBreakTime").value;
+    stopTimer()
+    saveData()
+}
+function saveData(){
+    let timeData = {};
+    timeData.uWTime = userWorkTime;
+    timeData.uBTime = userBreakTime;
+    timeData.uLBTime = userLongBreakTime;
+    localStorage.setItem("timeData", JSON.stringify(timeData))
+}
+function restoreData(){
+    changeTime()
+    let restoredTimeData = JSON.parse(localStorage.getItem("timeData"));
+    document.querySelector("#time").innerHTML = `${("0" + Math.floor(workTimer.time%86400%3600/60)).slice(-2)}:${("0" + workTimer.time%86400%3600%60).slice(-2)}`;
+    document.querySelector("#pomodoroTime").value = restoredTimeData.uWTime/60;
+    document.querySelector("#breakTime").value = restoredTimeData.uBTime/60;
+    document.querySelector("#longBreakTime").value = restoredTimeData.uLBTime/60;
+    changeTime()
+}
+// new Date().toJSON().slice(0,10)
 
-// let text = fs.readFileSync("data.json", "utf8")
-// console.log(text)
-
-
-
-const settingsCont = document.getElementById("settingsCont"),
+let settingsCont = document.getElementById("settingsCont"),
     settingsContent = document.querySelector(".settings-content"),
     settingsIcon = document.querySelector(".settings-icon"),
     closeSettings = document.querySelector(".close-settings");
- 
+
 settingsIcon.onclick = function() {
-  settingsCont.style.display = "block";
+    settingsCont.style.display = "block";
 } 
 closeSettings.onclick = function() {
-  settingsCont.style.animationName = "fadeOut";
-  settingsContent.style.animationName = "slideOutSettings";
-  setTimeout(function(){
-    settingsCont.style.animationName = "fadeIn"; 
-    settingsContent.style.animationName = "slideInSettings";
-    settingsCont.style.display = "none"; 
-  }, 400);
-}   
-window.onclick = function(event) {
-  if (event.target == settingsCont) {
     settingsCont.style.animationName = "fadeOut";
     settingsContent.style.animationName = "slideOutSettings";
     setTimeout(function(){
-      settingsCont.style.animationName = "fadeIn"; 
-      settingsContent.style.animationName = "slideInSettings";
-      settingsCont.style.display = "none"; 
+        settingsCont.style.animationName = "fadeIn"; 
+        settingsContent.style.animationName = "slideInSettings";
+        settingsCont.style.display = "none"; 
     }, 400);
-  }
+}   
+window.onclick = function(event) {
+    if (event.target == settingsCont) {
+        settingsCont.style.animationName = "fadeOut";
+        settingsContent.style.animationName = "slideOutSettings";
+        setTimeout(function(){
+            settingsCont.style.animationName = "fadeIn"; 
+            settingsContent.style.animationName = "slideInSettings";
+            settingsCont.style.display = "none"; 
+        }, 400);
+    }
 }
 
-
-
-
-var userWorkTime = 1500,
-    userBreakTime = 300,
-    userLongBreakTime = 900;
-
-function changeTime() { 
-  userWorkTime = document.querySelector("#pomodoroTime").value*60;
-  userBreakTime = document.querySelector("#breakTime").value*60;
-  userLongBreakTime = document.querySelector("#longBreakTime").value*60;
-  document.querySelector("#pomodoroTimeMin").innerHTML =  document.querySelector("#pomodoroTime").value;
-  document.querySelector("#breakTimeMin").innerHTML =  document.querySelector("#breakTime").value;
-  document.querySelector("#longBreakTimeMin").innerHTML =  document.querySelector("#longBreakTime").value;
-  stop()
-  saveData()
-}
-function saveData(){
-  let timeData = {};
-  timeData.uWTime = userWorkTime;
-  timeData.uBTime = userBreakTime;
-  timeData.uLBTime = userLongBreakTime;
-  localStorage.setItem("timeData", JSON.stringify(timeData))
-}
-
-function restoreData(){
-  let restoredTimeData = JSON.parse(localStorage.getItem("timeData"));
-  document.querySelector("#time").innerHTML = `${("0" + Math.floor(workTime%86400%3600/60)).slice(-2)}:${("0" + workTime%86400%3600%60).slice(-2)}`;
-  document.querySelector("#pomodoroTime").value = restoredTimeData.uWTime/60;
-  document.querySelector("#breakTime").value = restoredTimeData.uBTime/60;
-  document.querySelector("#longBreakTime").value = restoredTimeData.uLBTime/60;
-  changeTime()
-}
--- app/js/timer.js
+++ app/js/timer.js
@@ -1,125 +1,98 @@
 
 
-var workTime = userWorkTime -1,
-    breakTime = userBreakTime,
-    longBreakTime = userLongBreakTime,
-    setsNumber = 0;
+let pauseBt = document.querySelector("#pauseButton"),
+    stopBt = document.querySelector("#stopButton"),
+    breakBt = document.querySelector("#breakButton");
 
-var pauseBt = document.querySelector("#pauseButton"),
-      stopBt = document.querySelector("#stopButton");
+let mainTimer,
+    setsNum = 0;
+    nowGoing = "tobreak";
 
-var pomodoro;
-
-pauseBt.addEventListener('click', playWork)
-stopBt.addEventListener('click', stop)
-
-function setWork(){
-    document.querySelector("#time").innerHTML = `${("0" + Math.floor(workTime%86400%3600/60)).slice(-2)}:${("0" + workTime%86400%3600%60).slice(-2)}`;
-    workTime -=1;
-    if(workTime<0){
-        donePomodoro(1)
-        clearInterval(pomodoro);
-        workTime = userWorkTime;
-        breakTime = userBreakTime;
-        longBreakTime = userLongBreakTime;
-        // document.querySelector(".main-cont").style.background = "linear-gradient(-20deg, #df4e4e 32%, #fff0)";
-        document.querySelector(".main-cont").style.opacity = "0.7";
-        pauseBt.removeEventListener('click', playWork)
-        pauseBt.removeEventListener('click', pauseWork)
-        pauseBt.addEventListener('click', doneBreak)
-        pauseBt.childNodes[0].src = "static/images/timer-complete-big.png";
-        pomodoro = setInterval(setBreak, 1000)
-        setsNumber +=1
-        if(setsNumber>2){
-            donePomodoro(0)
-            setsNumber = 0
-            clearInterval(pomodoro)
-            document.querySelector(".main-cont").style.opacity = "0.3";
-            pomodoro = setInterval(setLongBreak, 1000)
+let workTimer = {
+        time: userWorkTime,
+    },
+    breakTimer = {
+        time: userBreakTime,
+    },
+    longBreakTimer = {
+        time: userLongBreakTime,
+    };
+function resetValues(){
+    workTimer.time = userWorkTime;
+    breakTimer.time = userBreakTime;
+    longBreakTimer.time = userLongBreakTime;
+};
+function setTimer(timer){
+    document.querySelector("#time").innerHTML = `${("0" + Math.floor(timer.time%86400%3600/60)).slice(-2)}:${("0" + timer.time%86400%3600%60).slice(-2)}`;
+    timer.time -=1;
+    if (timer.time < 0){
+        resetValues();
+        clearInterval(mainTimer)
+        if (nowGoing == "towork"){
+            mainTimer = setInterval(setTimer, 1000, workTimer);
+            nowGoing = "tobreak";
+            breakBt.style.display = "none";
+            pauseBt.style.display = "inline";
+            document.querySelector(".main-cont").style.opacity = "1";
+        } else {
+            donePomodoro(1);
+            setsNum ++;
+            nowGoing = "towork";
+            breakBt.style.display = "inline";
+            pauseBt.style.display = "none";
+            document.querySelector(".main-cont").style.opacity = "0.7";
+            if (setsNum >= 3){
+                donePomodoro(0);
+                setsNum = 0;
+                mainTimer = setInterval(setTimer, 1000, longBreakTimer);
+                document.querySelector(".main-cont").style.opacity = "0.4";
+            } else {
+                mainTimer = setInterval(setTimer, 1000, breakTimer)
+            }
         }
     }
 };
-function setBreak(){
-    document.querySelector("#time").innerHTML = `${("0" + Math.floor(breakTime%86400%3600/60)).slice(-2)}:${("0" + breakTime%86400%3600%60).slice(-2)}`;
-    breakTime -=1;
-    if(breakTime<0){
-        clearInterval(pomodoro);
-        workTime = userWorkTime;
-        breakTime = userBreakTime;
-        longBreakTime = userLongBreakTime;
-        // document.querySelector(".main-cont").style.background = "linear-gradient(-20deg, #514edf 36%, #c933c066)";
-        document.querySelector(".main-cont").style.opacity = "1";
-        pauseBt.removeEventListener('click', doneBreak)
-        pauseBt.addEventListener('click', pauseWork)
-        pauseBt.childNodes[0].src = "static/images/timer-pause-big.png";
-        pomodoro = setInterval(setWork, 1000)
-        console.log("Time to take a break!")
-    }
-}
-function setLongBreak(){
-    document.querySelector("#time").innerHTML = `${("0" + Math.floor(longBreakTime%86400%3600/60)).slice(-2)}:${("0" + longBreakTime%86400%3600%60).slice(-2)}`;
-    longBreakTime -=1;
-    if(longBreakTime<0){
-        clearInterval(pomodoro);
-        workTime = userWorkTime;
-        breakTime = userBreakTime;
-        longBreakTime = userLongBreakTime;
-        // document.querySelector(".main-cont").style.background = "linear-gradient(-20deg, #514edf 36%, #c933c066)";
-        document.querySelector(".main-cont").style.opacity = "1";
-        pauseBt.removeEventListener('click', doneBreak)
-        pauseBt.addEventListener('click', pauseWork)
-        pauseBt.childNodes[0].src = "static/images/timer-pause-big.png";
-        pomodoro = setInterval(setWork, 1000)
-    }    
-}
-function doneBreak(){
-    clearInterval(pomodoro);
-    document.querySelector(".main-cont").style.opacity = "1";
-    pauseBt.removeEventListener('click', doneBreak)
-    pauseBt.addEventListener('click', pauseWork)
-    pauseBt.childNodes[0].src = "static/images/timer-pause-big.png";
-    pomodoro = setInterval(setWork, 1000)
-}
-function pauseWork(){
-    clearInterval(pomodoro);
-    
+
+pauseBt.addEventListener('click', playTimer);
+stopBt.addEventListener('click', stopTimer);
+breakBt.addEventListener("click", clearBreak);
+
+function pauseTimer(){
+    clearInterval(mainTimer)
+    pauseBt.removeEventListener('click', pauseTimer);
+    pauseBt.addEventListener('click', playTimer);
     pauseBt.childNodes[0].src = "static/images/timer-start-big.png";
-    pauseBt.removeEventListener('click', pauseWork)
-    pauseBt.addEventListener('click', playWork)
     stopBt.style.display = 'inline';
-}
-function playWork(){
-    pomodoro = setInterval(setWork,1000)
-    workTime -=1
-    pauseBt.childNodes[0].src = "static/images/timer-pause-big.png";
-    pauseBt.removeEventListener('click', playWork)
-    pauseBt.addEventListener('click', pauseWork)
-    stopBt.style.display = 'none';
-}
-
-function stop(){
-    clearInterval(pomodoro);
-    workTime = userWorkTime;
-    breakTime = userBreakTime;
-    document.querySelector("#time").innerHTML = `${("0" + Math.floor(workTime%86400%3600/60)).slice(-2)}:${("0" + workTime%86400%3600%60).slice(-2)}`;
+};
+function playTimer(){
+    mainTimer = setInterval(setTimer, 1000, workTimer);
+    pauseBt.removeEventListener('click', playTimer);
+    pauseBt.addEventListener('click', pauseTimer);
     
-    pauseBt.childNodes[0].src = "static/images/timer-start-big.png";
-    pauseBt.addEventListener('click', playWork)
-    stopBt.style.display = "none"
-}
+    pauseBt.childNodes[0].src = "static/images/timer-pause-big.png";
+    stopBt.style.display = 'none';
+};
+function stopTimer(){
+    clearInterval(mainTimer);
+    resetValues();
 
-// const fs = require("fs");
-// let output = fs.createWriteStream("numbers.txt");
-// for(let i = 0; i < 100; i++) {
-// output.write(`${i}\n`);
-// }
-// output.end();
+    document.querySelector("#time").innerHTML = `${("0" + Math.floor(workTimer.time%86400%3600/60)).slice(-2)}:${("0" + workTimer.time%86400%3600%60).slice(-2)}`;
+    stopBt.style.display = 'none';
+};
+function clearBreak(){
+    clearInterval(mainTimer);
+    nowGoing = "tobreak";
+    mainTimer = setInterval(setTimer, 1000, workTimer);
+    document.querySelector("#time").innerHTML = `${("0" + Math.floor(workTimer.time%86400%3600/60)).slice(-2)}:${("0" + (workTimer.time+1)%86400%3600%60).slice(-2)}`;
+    breakBt.style.display = "none";
+    pauseBt.style.display = "inline";
+    document.querySelector(".main-cont").style.opacity = "1";
+};
 
 function donePomodoro(set){
     let pomodoroIcons = document.querySelectorAll(".done-cont .pomodoro img");
     if (set == 1){
-        try {pomodoroIcons[setsNumber].src = "static/images/pomodoro-done.png";}
-        catch (e){}//console.error(e)}
+        pomodoroIcons[setsNum].src = "static/images/pomodoro-done.png";
     } else if (set == 0){
         for (let icon of pomodoroIcons){
             icon.src = "static/images/pomodoro-unfinished.png";

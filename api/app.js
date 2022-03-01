aab922208d8d300ee04da5716ec3a3f68d0aaa0c
eles linux <eleswastaken@gmail.com>
2022-03-01 08:04:00 +0600

    create mongo config

--- api/app.js
+++ api/app.js
@@ -1,17 +1,20 @@
 
 
 const express = require('express');
-const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
 
+const initializeMongo = require('./mongoConfig');
+
 const app = express();
+
+initializeMongo()
 
 app.get('/', function(req, res) {
 	res.send('helluwwuwuwu');
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => 
 	console.log(`Running on http://localhost:${PORT}`)
 );

f164b1417bb48d6616e233249178639dd3038243
eles linux <eleswastaken@gmail.com>
2022-03-06 09:13:11 +0600

    implement log in submit handler

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,7 +14,6 @@ function App() {
 					<LogIn />
 				</Route>
 			</Switch>
-			<p> hello world! </p>
 		</Router>
 	);
 }
--- front-end/src/components/LogIn.js
+++ front-end/src/components/LogIn.js
@@ -3,11 +3,33 @@ import React from 'react';
 
 
 function LogIn() {
+	function handleSubmit(event) {
+		event.preventDefault();
+		const response = fetch('http://localhost:3000/log-in', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				email: event.target.email.value,
+				password: event.target.password.value
+			}),
+		});
+		response.then(res => res.json())
+			.then(res => localStorage.setItem('token', res.token));
+	}
 	return (
-		<form>
+		<form onSubmit={ handleSubmit }>
+			<h1>Log In</h1>
 			<label>
-				<input type="email" required />
+				<p>Email:</p>
+				<input type="text" name='email' required />
 			</label>
+			<label>
+				<p>Password:</p>
+				<input type="password" name='password' required />
+			</label>
+			<button type="submit">Log In</button>
 		</form>
 	);
 }

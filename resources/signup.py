f43e43b6348971a5c7bd665efa91ac519b66ced3
eles linux <eleswastaken@gmail.com>
2022-04-30 23:30:33 +0600

    Implement sign-up route

--- resources/signup.py
+++ resources/signup.py
@@ -1,7 +1,28 @@
 
 
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from models.user import User
+from helpers.jwt import createJWT
+
+def bcrypt(p):
+    return p
 
 class SignUp(Resource):
-    def get(self):
-        return {'message': 'this is a sign-up route!'}
+    # def get(self):
+    #     return {'message': 'this is a sign-up route!'}
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument(name='username', required=True)
+        parser.add_argument(name='email', required=True)
+        parser.add_argument(name='password', required=True)
+        args = parser.parse_args()
+
+        user = User(
+            username=args.username,
+            email=args.email,
+            password=bcrypt(args.password)
+        )
+        # user.save()
+
+        return {'token': createJWT(username=user.username, email=user.email)}

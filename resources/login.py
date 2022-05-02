bab768ddb77d3f893ddb1a07c9f617b2d47ad7e6
eles linux <eleswastaken@gmail.com>
2022-05-02 09:22:17 +0600

    Add bcrypt

--- resources/login.py
+++ resources/login.py
@@ -2,13 +2,9 @@
 from flask_restful import Resource, reqparse
 from models.user import Users
 from helpers.jwt import createJWT
-
-def bcrypt(p):
-    return p
+import bcrypt
 
 class LogIn(Resource):
-    # def get(self):
-    #     return {'message': 'this is a log-in route!'}
 
     def post(self):
 
@@ -16,12 +12,14 @@ class LogIn(Resource):
         parser.add_argument(name='email', required=True)
         parser.add_argument(name='password', required=True)
         args = parser.parse_args()
+
+        # user exists?
         try:
             user = Users.objects(email=args['email'])[0]
         except:
             return 
 
-        if bcrypt(user.password) == args['password']:
+        if bcrypt.checkpw(args['password'].encode(), user.password.encode()):
             # signed in
             return {'token': createJWT(username=user.username, email=user.email)}
 
--- resources/signup.py
+++ resources/signup.py
@@ -3,26 +3,27 @@
 from flask_restful import Resource, reqparse
 from models.user import Users
 from helpers.jwt import createJWT
-
-def bcrypt(p):
-    return p
+import bcrypt
 
 class SignUp(Resource):
-    # def get(self):
-    #     return {'message': 'this is a sign-up route!'}
 
     def post(self):
+        # parse request body
         parser = reqparse.RequestParser()
         parser.add_argument(name='username', required=True)
         parser.add_argument(name='email', required=True)
         parser.add_argument(name='password', required=True)
         args = parser.parse_args()
+        
+        # hash password
+        salt = bcrypt.gensalt()
+        password = bcrypt.hashpw(args['password'].encode(), salt)
 
         # new user
         user = Users(
             username=args['username'],
             email=args['email'],
-            password=bcrypt(args['password'])
+            password=password,
         )
         user.save()
 

25806c7b7c9fdd9f6837bc3017a9871320cd156d
eles linux <eleswastaken@gmail.com>
2022-05-06 16:30:30 +0600

    Create entries post

--- resources/entries.py
+++ resources/entries.py
@@ -1,5 +1,6 @@
 
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, reqparse
 from helpers.auth import authenticate
 
 class Entries(Resource):
@@ -7,3 +8,13 @@ class Entries(Resource):
 
     def get(self):
         return {'message': 'this is a entries route!'}
+
+    # create new entry
+    def post(self):
+        print('post entries')
+
+        json = request.get_json(force=True)
+
+        print(json)
+
+        return {'hello': 'there'}
